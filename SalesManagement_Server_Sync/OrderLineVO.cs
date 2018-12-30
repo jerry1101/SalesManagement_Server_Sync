9312487f227d2c26c4e60f31dbf88efd91bb19c4
Huei-Tze Hung <jerry110160@gmail.com>
2018-12-29 22:15:27 -0500

    Add VO and parsing request body

---
+++ SalesManagement_Server_Sync/OrderLineVO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement_Server_Sync
+{
+    class OrderLineVO
+    {
+        public int SKU { get; set; }
+        public int quantity { get; set; }
+
+        public static explicit operator List<object>(OrderLineVO v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SalesManagement_Server_Sync/PlaceOrder.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace SalesManagement_Server_Sync
+{
+    public static class PlaceOrder
+    {
+        [FunctionName("AddOrder")]
+        public static async Task<HttpResponseMessage> AddOrder([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, 
+            TraceWriter log)
+        {
+            log.Info("Processing a AddOrder request.");
+            string _requestBody = await req.Content.ReadAsStringAsync();
+            
+
+            if (_requestBody == null)
+            {
+                // Get request body
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Please pass data info on the query string or in the request body");
+            }
+            else
+            {
+                var _OrderLines = JsonConvert.DeserializeObject<List<OrderLineVO>>(_requestBody);
+                return _OrderLines.Count < 1
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass skus in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello order id is 1111");
+            }
+
+            
+        }
+    }
+}
